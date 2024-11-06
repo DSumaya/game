8e97834aa7943ac366dc6b42145cefd80a661fe1
DSumaya <summa320808@gmail.com>
2024-11-06 22:49:26 +0600

    hw_2.py

--- hw_2.py
+++ hw_2.py
@@ -17,6 +17,7 @@ class Square(Figure):
         square_area = self.calculate_area()
         print(f'Square side length: {self.__side_length} {self.unit}, Area: {square_area} {self.unit}')
 
+
 class Rectangle(Figure):
     def __init__(self, length, width):
         self.__length = length
@@ -30,14 +31,11 @@ class Rectangle(Figure):
         print(f'Rectangle length: {self.__length} {self.unit}, Rectangle width: {self.__width} {self.unit} '
               f'Area: {rectangle_area} {self.unit}')
 
-squares = [Square(5), Square(9)]
-rectangles = [Rectangle(4, 5), Rectangle(3,9), Rectangle(6,2)]
-for figure in squares:
-    figure.info()
 
-for figure in rectangles:
+figure_list =[Square(8), Square(4),
+              Rectangle(4, 5), Rectangle(3,9), Rectangle(6,2)]
+for figure in figure_list:
     figure.info()
-
 
 
 
